2988051448155d0e82182e8026842d6210d8349a
ShubhamLuthtra <shubh.luthra20@bvicam.in>
2022-04-21 16:07:41 +0530

    shopping-cart(using react-redux)

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,16 +1,44 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { authActions } from "../store/auth-slice";
 
 import "./Auth.css";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(authActions.login());
+  };
+
+  const [data, setData] = useState({ email: "", password: "" });
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
   return (
     <div className="container">
       <h1>Login</h1>{" "}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="id">Id</label>
-        <input type="text" name="id" id="id" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={data.email}
+          required
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={data.password}
+          required
+        />
         <button className="login-btn" type="submit">
           Login
         </button>
